b6563b6280857c2d64e3b41fea611ef632f9942c
Henrique Lima <askrique@gmail.com>
2025-08-14 03:46:25 +0000

    Altera para a fonte Grand Hotel, deie a fonte maior

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['"Londrina Shadow"', 'sans-serif'],
+        headline: ['"Grand Hotel"', 'cursive'],
         code: ['monospace'],
       },
       colors: {
