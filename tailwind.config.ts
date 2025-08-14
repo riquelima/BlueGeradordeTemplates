db0f9f35b5037ee7c3536a3c1c9ea9e8f75b90a2
Henrique Lima <askrique@gmail.com>
2025-08-14 03:45:34 +0000

    Utilize a fonte londrina shadow em Buteco Pé de Serra - Gerador de Templ

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['"Lilita One"', 'sans-serif'],
+        headline: ['"Londrina Shadow"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
