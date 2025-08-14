278beaa559f50efe659eb51bc3cd0fa5222ba4e2
Henrique Lima <askrique@gmail.com>
2025-08-14 03:47:28 +0000

    Deixe Buteco Pé de Serra - Gerador de Templates alinhado ao meio do sist

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <div className="flex flex-col h-screen bg-transparent text-foreground">
       <header className="px-6 py-2 border-b border-black/10">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-center">
           <div className="flex items-center gap-4">
             <img src="https://raw.githubusercontent.com/riquelima/BostonProCleaning/refs/heads/main/Nova-Logo-PDS-2024-02.png" alt="Buteco Pé de Serra Logo" className="h-24" />
             <h1 className="text-4xl font-headline tracking-wider">Buteco Pé de Serra - Gerador de Templates</h1>
